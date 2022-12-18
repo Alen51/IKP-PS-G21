97dec797ed843522674a11f525b5c8c14f29c8ae
Alen Eminović <eminovic.pr51.2019@uns.ac.rs>
2022-12-18 21:41:57 +0100

    initial commit

--- Subscriber/Subscriber.cpp
+++ Subscriber/Subscriber.cpp
@@ -1,4 +1,4 @@
-// Publisher.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Subscriber.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -6,7 +6,6 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "hi";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
